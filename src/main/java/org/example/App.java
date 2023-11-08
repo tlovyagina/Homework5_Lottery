cb8e183daa03f63b90bbef2f0bf38645e8d65e2d
tlovyagina <tatiana.loviagina@yandex.ru>
2023-11-08 20:28:05 +0300

    Final commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,8 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Lottery lottery = new Lottery();
+        System.out.println("Результаты тура лотереи: " + lottery.roll());
     }
 }
---
+++ src/main/java/org/example/Lottery.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lottery {
+    public List<Integer> roll() {
+        List<Integer> balls = new ArrayList<>(100);
+        for (int i = 0; i < 100; i++) {
+            balls.add(i + 1);
+        }
+        Collections.shuffle(balls);
+        return balls.subList(0, 10);
+    }
+}
